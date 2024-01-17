c095c5dcf138b90151de5699cd33ec6c2c0d550d
AdeenWaqar <156835776+AdeenWaqar@users.noreply.github.com>
2024-01-17 16:21:39 +0500

    Add files via upload

---
+++ Adeen.py
@@ -0,0 +1,18 @@
+x=int(input("enter no 1"))
+operator=input("enter any operator:")
+y=int(input("enter no 2"))
+if operator == "+":
+          print(x+y)
+elif operator == "-":
+    print(x-y)
+elif operator == "*":
+    print(x*y)
+elif operator =="/":
+    print(x/y)
+else:
+    print("invalid")
+    
+
+
+
+          
